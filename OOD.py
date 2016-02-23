3b3361a796aa97815ac0552998351900616e1113
dunkyfool <dunkyfool0113@gmail.com>
2016-02-23 18:09:44 +0800

    complete loadData(build trainData)

--- OOD.py
+++ OOD.py
@@ -1,12 +1,16 @@
 #--------------------------------------------------------------------------
 #
-#  Project Nmae: mlp.py
-#  Function: Modelize MULTI-LAYER Neural Network
+#  Project Nmae: OOD.py
+#  Goal: Build a single neural network to do object detection & classify the image
 #
 #
-#  Date: 2015.12.25
+#  Date: 2016.02.23
 #  Author: Jackie
 #--------------------------------------------------------------------------
+#  Warning:
+#  loadData lacks load trainData
+#  shuffle not working
+#  image size in this code and dataBuilder must be the same
 import theano, theano.tensor as T
 import scipy.io as sio
 import numpy as np
@@ -17,6 +21,8 @@ import timeit, cPickle, pylab
 from PIL import Image
 from tabulate import tabulate
 import matplotlib.pyplot as plt
+import cv2
+import os
 
 ##########################
 #   Actvation Function   #
@@ -141,25 +147,44 @@ class MLP(object):
     self.params = self.L1.params + self.L2.params
     self.output = self.L2.output
 
-def loadData(filename,grid_num,class_num):
+def loadData(filename,grid_num,class_num,img_size):
   #trainLabels(training_num, grid_sq, xywhC)
+  #trainData will follow img_label to build the trainData
   label = np.loadtxt(filename,dtype='str')
   grid_sq = grid_num **2
+  trainData = []
   trainLabels = []
   #print label; print label.shape
   for i in range(label.shape[0]):
+    img = cv2.imread(os.path.join('data/',label[i][0]))
+    img = cv2.resize(img,(img_size,img_size))
+    #cv2.imshow('img',img)
+    #cv2.waitKey(0) & 0xFF
+    #cv2.destroyAllWindows()
     xywh = np.repeat(label[i][1:5].reshape((1,4)),grid_sq,axis=0)
     #print xywh, xywh.shape
     clas = np.repeat(label[i][5+grid_sq:].reshape((1,class_num)),grid_sq,axis=0)
     #print clas, clas.shape
     tmp = np.concatenate((xywh,label[i][5:5+grid_sq].reshape((grid_sq,1)),clas),axis=1)
     #print tmp, tmp.shape
+    #print img.shape
+    trainData += [img.flatten()]
     trainLabels += [tmp]
+  trainData = np.asarray(trainData)
   trainLabels = np.asarray(trainLabels).reshape((label.shape[0],grid_sq,5+class_num))
-  print trainLabels, trainLabels.shape
+  #print trainLabels, trainLabels.shape
+  #print trainData, trainData.shape
+  return trainData, trainLabels
 
-def trainingNetwork():
-  pass
+
+def trainNetwork(g,v,trainData,trainLabels,epoch,epoch_num):
+  start_time = timeit.default_timer()
+  for e in range(epoch_num):
+    for i in range(trainData.shape[0]/batch_size):
+      y,c = g(trainData[i*batch_size:(i+1)*batch_size],
+              trainLabels[i*batch_size:(i+1)*batch_size,:,:])
+    #shuffle(train)
+
 
 def test_mlp(bs,nu,lr,fs,ep,l1,l2,wd):
   ##########################
@@ -212,60 +237,7 @@ def test_mlp(bs,nu,lr,fs,ep,l1,l2,wd):
 #    Training Model      #
 ##########################
 
-  print "start testing"
-  start_time = timeit.default_timer()
-  for e in range(epoch_num):
-    for i in range(trainData.shape[0]/batch_size):
-      y,c = g(trainData[i*batch_size:(i+1)*batch_size],trainLabel[i*batch_size:(i+1)*batch_size])
-      # print("Epoch=%d, Progress: %.2f%%, cost=%.2f" %(e+1, i*100./(trainData.shape[0]/batch_size),c))
-    shuffle(trainData,trainLabel,trainData.shape[1])
-    ### Validation on EVERY FIVE EPOCHS [START]
-    if (e+1)%10 == 0:
-        dis_trainLabels_correct=np.zeros(101)
-        dis_testLabels_correct=np.zeros(101)
-        trainCorrect=0
-        trainCtr=0
-        for k in range(trainData.shape[0]/batch_size):
-          trainY1 = valid_model(trainData[k*batch_size:(k+1)*batch_size])
-          for j in range(batch_size):
-            if np.argmax(trainLabel[trainCtr]) == np.argmax(trainY1[0][j]):
-              trainCorrect+=1
-              # Record each class's accuracy
-              dis_trainLabels_correct[np.argmax(trainLabel[trainCtr])]+=1
-            trainCtr+=1
-        testCorrect=0
-        testCtr=0
-        testWrongClass=[[]]*101
-        misRecognizedClass=[0]*101
-        for k in range(testData.shape[0]/batch_size):
-          testY1 = valid_model(testData[k*batch_size:(k+1)*batch_size])
-          for j in range(batch_size):
-            if np.argmax(testLabel[testCtr]) == np.argmax(testY1[0][j]):
-              testCorrect+=1
-              # Record each class's accuracy
-              dis_testLabels_correct[np.argmax(testLabel[testCtr])]+=1
-            else:
-              # Record wrong test result
-              correctLabel = np.argmax(testLabel[testCtr])
-              wrongLabel = np.argmax(testY1[0][j])
-              flag=0
-              misRecognizedClass[wrongLabel] = misRecognizedClass[wrongLabel]+1
-              if len(testWrongClass[correctLabel]) == 0:
-                testWrongClass[correctLabel] = testWrongClass[correctLabel]+[[wrongLabel,1]]
-              else:
-                for b in range(len(testWrongClass[correctLabel])):
-                  if testWrongClass[correctLabel][b][0] == wrongLabel:
-                    testWrongClass[correctLabel][b][1]+=1
-                    flag=1
-                    break
-                if flag == 0:
-                  testWrongClass[correctLabel] = testWrongClass[correctLabel]+[[wrongLabel,1]]
-            testCtr+=1
-
-        print("e=%3d Total = %d,train Correct_rate: %.2f%% test Correct_rate: %.2f%%" %(e+1,trainCtr,trainCorrect*100./trainCtr,testCorrect*100./testCtr))
-
-        record('20160111_auto_cnn_dnn_log',2,(e+1,trainCorrect*100./trainCtr,batch_size,neuron,learning_rate,filter_size,lambda1,lambda2,testCorrect*100./testCtr))
-
+  trainNetwork()
 #        if trainCorrect*100./trainCtr > good_record:
 #          good_record = trainCorrect*100./trainCtr
 #          file_name = str(bs)+'_'+str(nu)+'_'+str(lr)+'_'+str(fs)+'_para'
@@ -277,42 +249,9 @@ def test_mlp(bs,nu,lr,fs,ep,l1,l2,wd):
 #                              cnn.w.get_value(),
 #                              cnn.b.get_value()])
 ### Validation on EVERY FIVE EPOCHS [END]
-  end_time = timeit.default_timer()
-
-  print('Total time: %.2f' % ((end_time-start_time)/60.))
 
 
 # def trail_test(bs,nu,lr,fs):
-#   #rebuild a new one
-#   mat = sio.loadmat('caltech101_silhouettes_16')
-#   train = mat['X']
-#   ans = mat['Y'].reshape(train.shape[0])
-#   mx = np.identity(101)
-#   label=[]
-#   for key in range(train.shape[0]):
-#     label.append(mx[ans[key]-1])
-#   label = np.array(label)
-
-#   x = T.matrix('x')
-#   y_hat = T.matrix('y_hat')
-#   batch_size = bs
-#   epoch_num = 20
-#   neuron = nu
-#   learning_rate = lr
-#   filter_size = fs
-#   image_size = 16
-#   cnn_output_size = pow((image_size-filter_size+1)/2,2)
-
-#   cnn_input = x.reshape((batch_size,1,image_size,image_size))
-#   cnn = CNN_Layer(cnn_input,filter_shape=(1,1,filter_size,filter_size),
-#                   image_shape=(batch_size,1,image_size,image_size),
-#                   poolsize=(2,2))
-
-#   dnn_input = cnn.output.reshape((batch_size,cnn_output_size))
-#   dnn = MLP(dnn_input,y_hat,cnn_output_size,neuron,101,batch_size)
-
-#   trail_model=theano.function(inputs=[x],outputs=[dnn.output])
-
 #   print 'Load w and b...'
 #   save_file = open('500_512_0.001_3_para')
 #   dnn.L1.w.set_value(cPickle.load(save_file))
@@ -323,19 +262,9 @@ def test_mlp(bs,nu,lr,fs,ep,l1,l2,wd):
 #   cnn.b.set_value(cPickle.load(save_file))
 #   save_file.close()
 
-#   correct=0
-#   ctr=0
-#   for i in range(train.shape[0]/batch_size):
-#     y1 = trail_model(train[i*batch_size:(i+1)*batch_size])
-#     for j in range(batch_size):
-#       if np.argmax(label[ctr])+1 == np.argmax(y1[0][j])+1:
-#           correct+=1
-#       ctr+=1
-
-#   print("Total = %d, Correct_rate: %.2f%%" %(ctr,correct*100./ctr))
-
 
 if __name__ == '__main__':
-#  loadData('img_label',4,2)
-  test_mlp(50,128,0.06,5,1000,0,0,0)
+  loadData('img_label',4,2,480)
+#  trainNetwork()
+#  test_mlp(50,128,0.06,5,1000,0,0,0)
 #  trail_test(1,512,0.001,3)
